8910b743de029ef46b1bc990072ca2418422dc10
Shawna Huang <shawna_huang@brown.edu>
2017-11-14 18:18:02 -0500

    deleted excess in List.jsx

--- src/List.jsx
+++ src/List.jsx
@@ -10,44 +10,6 @@ import Movie from "./Movie";
 */
 class List extends Component {
 
-
-    findMonth(number) {
-      if (number == 1) {
-        return "January";
-      } else if (number == 2) {
-        return "February";
-      } else if (number == 3) {
-        return "March";
-      } else if (number == 4) {
-        return "April";
-      } else if (number == 5) {
-        return "May";
-      } else if (number == 6) {
-        return "June";
-      } else if (number == 7) {
-        return "July";
-      } else if (number == 8) {
-        return "August";
-      } else if (number == 9) {
-        return "September";
-      } else if (number == 10) {
-        return "October";
-      } else if (number == 11) {
-        return "November";
-      } else if (number == 12) {
-        return "December";
-      }
-    }
-
-    findBoxOffice(num) {
-      if (num > 1000) {
-        return (num / 1000) + " billion";
-      } else {
-        return num + " million";
-      }
-    }
-
-
     renderList() {
         const items = this.props.items.map(item => {
           return <Movie item={item} key={item.name} />
